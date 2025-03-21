6b1064af4bd83ed424c1ff0b96245610d6de1e7d
GitHub Action <action@github.com>
2025-03-21 12:34:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/azimsh3r/gitammendcommitplugin/MyBundle.kt
+++ src/main/kotlin/com/github/azimsh3r/gitammendcommitplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.azimsh3r.gitammendcommitplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/azimsh3r/gitammendcommitplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/azimsh3r/gitammendcommitplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.azimsh3r.gitammendcommitplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
